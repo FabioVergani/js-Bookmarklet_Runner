f86c51e996ccbde5febe01f10544b0b93d143fef
FabioVergani <fabio.vergani.1975@gmail.com>
2015-08-15 20:57:32 +0200

    Update runscript.js

--- runscript.js
+++ runscript.js
@@ -1,11 +1,15 @@
 void((function($w){'use strict';
  var e, o=$w, p='NameSpaceUniqueId';
  if((p in o) && o.hasOwnProperty(p) && (e=o[p]) instanceof Function){
-
-
+	e(o,2);
  }else if(!e||Object.getOwnPropertyDescriptor(o,p).writable){
-	e=o[p]='test';
+
+	(e=o[p]=function scope(global,i){
+
+		log(i);
+
+	})(o,1);
+
  };
- e=null;
 })(window));
-//END.
+

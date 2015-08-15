717f45fa6f2f71fb7bc7b0c6879f6ec601fc8bdc
FabioVergani <fabio.vergani.1975@gmail.com>
2015-08-15 18:50:37 +0200

    Create runscript.js

---
+++ runscript.js
@@ -0,0 +1,11 @@
+void((function($w){'use strict';
+ var e, o=$w, p='NameSpaceUniqueId';
+ if((p in o) && o.hasOwnProperty(p) && (e=o[p]) instanceof Function){
+
+
+ }else if(!e||Object.getOwnPropertyDescriptor(o,p).writable){
+	e=o[p]='test';
+ };
+ //e(o);
+})(window));
+//END.
